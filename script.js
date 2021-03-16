37af903b5612bcc06881e348202e2a8c607dbdba
Nagarajan <46449220+Raj2710@users.noreply.github.com>
2021-03-16 09:36:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var value = 10
+var value = 11
 function timer()
 {
     var ele = document.getElementById("root");
-    ele.innerHTML=value--;
+    ele.innerHTML=--value;
     if(value == 0)
     {
         ele.innerHTML="Happy Independance Day!!!";
-        value=10;
+        value=11;
         return;
     }
     setTimeout(timer, 1000);
